0f0c6db92092f14da65f194362200cabb7224a9a
genre0928 <87413846+genre0928@users.noreply.github.com>
2024-02-25 20:18:39 +0900

    재생, 일시정지, 키보드 색 변화 구현
    - 타이머의 재생, 일시정지 기능 구현
    - 블럭 결과에 따른 키보드 색 입력 구현

--- js/index.js
+++ js/index.js
@@ -2,10 +2,11 @@
 const answer = "APPLE";
 let attempts = 0;
 let index = 0;
+let pausedTime = 0;
 
 document.addEventListener("DOMContentLoaded", function () {
   // 타이머 업데이트 함수
-  const starttime = new Date();
+  let starttime = new Date();
   function cntTime() {
     const currentTime = new Date();
     const runningTime = new Date(currentTime - starttime);
@@ -19,16 +20,34 @@ document.addEventListener("DOMContentLoaded", function () {
       timerElement.innerText = timeString;
     }
   }
-  // 1분마다 타이머 업데이트
+
+  const playTime = document.querySelector(".btn-media__play-img");
+  const pauseTime = document.querySelector(".btn-media__pause-img");
+
+  playTime.addEventListener("click", timePlay);
+  pauseTime.addEventListener("click", timePause);
+
+  function timePause() {
+    clearInterval(timer);
+    pausedTime = new Date() - starttime;
+    console.log("일시정지");
+  }
+
+  function timePlay() {
+    starttime = new Date() - pausedTime;
+    timer = setInterval(cntTime, 1000);
+    console.log("재생");
+  }
+
+  // 1초마다 타이머 업데이트
   timer = setInterval(cntTime, 1000);
 });
 
 function appStart() {
   const displayGameover = () => {
     const div = document.createElement("div");
+    div.classList.add("game-over");
     div.innerText = "게임종료";
-    div.style =
-      "display:flex; justify-content:center; align-items:center; position:absolute; top:40vh; left: 45vw";
     document.body.appendChild(div);
     clearInterval(timer);
   };
@@ -61,14 +80,24 @@ function appStart() {
       const block = document.querySelector(
         `.board-block[data-index='${attempts}${i}']`
       );
-      const inputWord = block.innerText;
+      const keyBlock = document.getElementById(block.innerText.toUpperCase());
       const answerWord = answer[i];
+      const inputWord = block.innerText;
+      block.classList.add("check-answer");
       if (inputWord === answerWord) {
         cntCorrect += 1;
-        block.style.background = "#6aaa64";
-      } else if (answer.includes(inputWord)) block.style.background = "#C9B458";
-      else block.style.background = "#787c7e";
-      block.style.color = "white";
+        block.classList.add("corrected");
+        keyBlock.className = "key-block";
+        keyBlock.classList.add("corrected");
+      } else if (answer.includes(inputWord)) {
+        block.classList.add("included");
+        keyBlock.className = "key-block";
+        keyBlock.classList.add("included");
+      } else {
+        block.classList.add("wronged");
+        keyBlock.className = "key-block";
+        keyBlock.classList.add("wronged");
+      }
     }
     if (cntCorrect === 5) gameover();
     else nextLine();
@@ -94,6 +123,16 @@ function appStart() {
 }
 
 appStart();
+
+window.addEventListener("keydown", (e) => {
+  const key = document.getElementById(e.key.toUpperCase());
+  if (key) key.classList.add("pressed");
+});
+
+window.addEventListener("keyup", (e) => {
+  const key = document.getElementById(e.key.toUpperCase());
+  if (key) key.classList.remove("pressed");
+});
 
 // 그 언젠가 이걸로도 완성해보기
 // window.addEventListener("keydown", (e) => {
